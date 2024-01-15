84b99800a2f9228662f9ecf0ba56d01e36587c61
ritvikvenkateshwar <156514183+ritvikvenkateshwar@users.noreply.github.com>
2024-01-15 19:48:28 +0000

    Add files via upload
    The code to make the deep learning model

---
+++ BTYS_2025.py
@@ -0,0 +1,15 @@
+#import the required libraries
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sea
+import warnings
+
+#import the data
+
+#format the data
+
+#train the model
+
+#validate the model
