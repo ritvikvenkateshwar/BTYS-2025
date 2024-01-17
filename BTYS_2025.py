dccce5d798170dcf28a8a5ea70d3ba090ec2e364
ritvikvenkateshwar <156514183+ritvikvenkateshwar@users.noreply.github.com>
2024-01-17 18:47:51 +0000

    Update BTYS_2025.py
    added base code to convert to TPU

--- BTYS_2025.py
+++ BTYS_2025.py
@@ -13,3 +13,16 @@ import warnings
 #train the model
 
 #validate the model
+
+#convert to TFlite
+saved_keras_model = 'model.h5'
+model.save(saved_keras_model)
+
+converter = tf.lite.TFLiteConverter.from_keras_model_file(saved_keras_model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model = converter.convert()
+
+with open('mobilenet_v2_1.0_224.tflite', 'wb') as f:
+  f.write(tflite_model)
+
+#Convert to Tensor Processing Unit(hehe cute box)
