34130ede41703a5760333bfef0b5d97f179f593a
ritvikvenkateshwar <156514183+ritvikvenkateshwar@users.noreply.github.com>
2024-06-21 13:26:39 +0100

    Update BTYS_2025.py

--- BTYS_2025.py
+++ BTYS_2025.py
@@ -13,9 +13,8 @@ from PIL import Image, ImageDraw
 
 
 #import the data
-
 image = Image.open("C:\\Users\\vvenkata\\Desktop\\coding stuff\\BTYS_2025\\Eye Scans\\Independent Test Set\\Independent Test Set\\Keratoconus\\case1\\KCN_1_CT_A.jpg")
-imgplot = plt.imshow(img)
+image.show()
 #format the data
 
 #train the model
@@ -23,15 +22,5 @@ imgplot = plt.imshow(img)
 #validate the model
 
 #convert to TFlite
-"""saved_keras_model = 'model.h5'
-model.save(saved_keras_model)
-
-converter = tf.lite.TFLiteConverter.from_keras_model_file(saved_keras_model)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-tflite_model = converter.convert()
-
-with open('mobilenet_v2_1.0_224.tflite', 'wb') as f:
-  f.write(tflite_model)"""
 
 #Convert to Tensor Processing Unit(hehe cute box)
-
